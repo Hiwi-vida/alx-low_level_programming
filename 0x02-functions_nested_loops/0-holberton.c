2727f6499ce613f7eebc9ee871be1d100eb265e3
Hiwot <85571842+Hiwi-vida@users.noreply.github.com>
2021-09-01 17:26:01 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *word = "Holberton";
+
+	while (*word)
+	{
+		_putchar(*word);
+		word++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
